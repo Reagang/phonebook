352b58980d4416796095ad49029b7fc9b5e49db4
Reagan Gallant <ReaganG@marshan.co.za>
2020-06-28 21:29:49 +0200

    Initial commit - Create API Project -Add EntityFrameworkCore -Add Initial Migration -Add Person Model

---
+++ API/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class Person
+    {
+        public virtual int Id { get; set; }
+
+        public virtual string Name { get; set; }
+
+        public virtual string Surname { get; set; }
+        public DateTime CreationTime { get; set; }
+        public virtual bool IsActive { get; set; }
+    }
+}
---
+++ API/Models/Phone.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class Phone
+    {
+        public virtual int PersonId { get; set; }
+        public virtual string Number { get; set; }
+        public virtual Person Person { get; set; }
+        public DateTime CreationTime { get; set; }
+        public virtual bool IsActive { get; set; }
+    }
+}
